7bc47e46ce0e02b127ad083cd40251d6021ae43a
maragramonte <agramontemar@gmail.com>
2025-02-07 08:43:32 +0100

    MatrizVerificacion.java

--- 2º TRIMESTRE/1º EXAMEN/MatrizVerificacion.java
+++ 2º TRIMESTRE/1º EXAMEN/MatrizVerificacion.java
@@ -1,4 +1,39 @@
+/* Algoritmo verificarDiagonales
+    Entrada: matriz (una matriz bidimensional de enteros)
+    Salida: Verdadero si los elementos de la primera columna y la última fila son iguales entre sí, Falso en caso contrario
 
+    Si matriz es nula o matriz está vacía o la primera fila de la matriz está vacía
+        Retornar Falso
+    Fin Si
+
+    Crear conjunto primeraColumna vacío
+    Crear conjunto ultimaFila vacío
+
+    Para cada fila en la matriz hacer
+        Agregar el primer elemento de la fila al conjunto primeraColumna
+    Fin Para
+
+    Asignar el índice de la última fila de la matriz a ultimaFilaIndex
+
+    Para cada elemento en la última fila de la matriz hacer
+        Agregar el elemento de la última fila al conjunto ultimaFila
+    Fin Para
+
+    Si el tamaño de primeraColumna es 1 y el tamaño de ultimaFila es 1
+        Retornar Verdadero
+    Sino
+        Retornar Falso
+    Fin Si
+Fin Algoritmo
+
+
+Algoritmo principal
+    Imprimir verificarDiagonales([[1, 2, 3], [1, 2, 3], [1, 2, 3]])  // Debería imprimir Verdadero
+    Imprimir verificarDiagonales([[1, 2, 3], [4, 5, 6], [7, 8, 9]])  // Debería imprimir Falso
+    Imprimir verificarDiagonales([[2, 2, 2], [2, 2, 2], [2, 2, 2]])  // Debería imprimir Verdadero
+    Imprimir verificarDiagonales([[1, 2], [1, 2], [1, 3]])  // Debería imprimir Falso
+Fin Algoritmo
+ */
 import java.util.HashSet;
 import java.util.Set;
 
